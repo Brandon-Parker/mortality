375c546b3de6a11dbc7e40197558a9635a745c57
BRANDON PARKER <Brandon@dkparker.com>
2015-11-16 14:36:05 -0800

    fixed bug with links for changing between pages(missing closing a tag)

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// function store() {
+//   var submitEl = document.getElementById('nameInput');
+//   localStorage.setItem("name", submitEl.value);
+// }
+
+
+// submitEl.addEventListener('click', submitEl);

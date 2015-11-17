f54655a1f5b10d97e2f0803c29088d7f6b40d261
Carson Daniel Parker <Carson@dkparker.com>
2015-11-16 16:38:53 -0800

    added another form for noun, adj, and verb input

--- js/index.js
+++ js/index.js
@@ -5,3 +5,13 @@
 
 
 // submitEl.addEventListener('click', submitEl);
+
+var noun1 = [];
+var noun2 = [];
+var noun3 = [];
+var adjective1 = [];
+var adjective2 = [];
+var adjective3 = [];
+var verb1 = [];
+var verb2 = [];
+var verb3 = [];

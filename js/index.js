66f037a37933be5f7ef3bab5a301d2969e86054b
Melissa Giaime <melissa.giaime@gmail.com>
2015-11-17 10:31:34 -0800

    fixed all JS issues with local storage and name display on linked pages with peer programming MLG BP

--- js/index.js
+++ js/index.js
@@ -1,17 +1,28 @@
-// function store() {
-//   var submitEl = document.getElementById('nameInput');
-//   localStorage.setItem("name", submitEl.value);
-// }
+function storeData() {
+  var inputEl = document.getElementById("input").value;
+  localStorage.setItem("name", JSON.stringify(inputEl));
+  console.log(inputEl);
+}
 
+function getDataOut() {
+  var fill = document.getElementById("getUserInput");
+  var getData = localStorage.getItem("name");
+  var parsedData = JSON.parse(getData);
+  console.log(parsedData);
+  fill.textContent = parsedData;
+}
 
-// submitEl.addEventListener('click', submitEl);
+var buttonAction = document.getElementById("button");
+buttonAction.addEventListener("click", storeData);
 
-var noun1 = [];
-var noun2 = [];
-var noun3 = [];
-var adjective1 = [];
-var adjective2 = [];
-var adjective3 = [];
-var verb1 = [];
-var verb2 = [];
-var verb3 = [];
+// var noun1 = [];
+// var noun2 = [];
+// var noun3 = [];
+// var adjective1 = [];
+// var adjective2 = [];
+// var adjective3 = [];
+// var verb1 = [];
+// var verb2 = [];
+// var verb3 = [];
+
+getDataOut();

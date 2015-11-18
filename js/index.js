7494ff94ef4b6bd13a9d4f92ba76f3e995fc6de3
Melissa Giaime <melissa.giaime@gmail.com>
2015-11-17 20:53:51 -0800

    Added in JS code to allow user inputs to populate the madlib templates and display on the results.html page. The JS additions are not functional at this time, but are not far from the finish line

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ function storeData() {
   var inputEl = document.getElementById("input").value;
   localStorage.setItem("name", JSON.stringify(inputEl));
   console.log(inputEl);
+
+  var theStory = document.getElementById("resultForm");
+  theStory.innerHTML = plugInMadLibs(getSelectedStory(),getUserChoices());
 }
 
 function getDataOut() {
@@ -9,23 +12,64 @@ function getDataOut() {
   var getData = localStorage.getItem("name");
   var parsedData = JSON.parse(getData);
   console.log(parsedData);
-  fill.textContent = parsedData;
+  //fill.textContent = parsedData;
+}
+
+function getSelectedStory() {
+  //http://stackoverflow.com/questions/9618504/get-radio-button-value-with-javascript
+  var radios = document.getElementsByName('mood');
+
+  for (var i = 0; i < radios.length; i++) {
+      if (radios[i].checked) {
+          // return the selected story without madlibs plugged in
+          return stories[radios[i].id];
+
+          // only one radio can be logically checked, don't check the rest
+          break;
+      }
+  }
+}
+
+function getUserChoices() {
+
+  var listOfChoices = {
+    nounList: [],
+    adjectiveList: [],
+    verbList: []
+  }
+
+  var nouns = document.getElementsByClassName("noun");
+  for (var i = 0; i < nouns.length; i++) {
+    listOfChoices.nounList.push( nouns[i].value );
+  }
+
+  var adjectives = document.getElementsByClassName("adj");
+  for (var i = 0; i < adjectives.length; i++) {
+    listOfChoices.adjectiveList.push( adjectives[i].value );
+  }
+
+  var verbs = document.getElementsByClassName("verb");
+  for (var i = 0; i < verbs.length; i++) {
+    listOfChoices.verbList.push( verbs[i].value );
+  }
+
+  return listOfChoices;
+}
+
+function plugInMadLibs (story, listOfChoices) {
+  story.replace("[verb1]",listOfChoices.verbList[0]);
+  story.replace("[verb2]",listOfChoices.verbList[1]);
+  story.replace("[verb3]",listOfChoices.verbList[2]);
+  story.replace("[noun1]",listOfChoices.nounList[0]);
+  story.replace("[noun2]",listOfChoices.nounList[1]);
+  story.replace("[noun3]",listOfChoices.nounList[2]);
+  story.replace("[adj1]",listOfChoices.adjectiveList[0]);
+  story.replace("[adj2]",listOfChoices.adjectiveList[1]);
+  story.replace("[adj3]",listOfChoices.adjectiveList[2]);
+  return story;
 }
 
 var buttonAction = document.getElementById("button");
 buttonAction.addEventListener("click", storeData);
 
-// var noun1 = [];
-// var noun2 = [];
-// var noun3 = [];
-// var adjective1 = [];
-// var adjective2 = [];
-// var adjective3 = [];
-// var verb1 = [];
-// var verb2 = [];
-// var verb3 = [];
-
 getDataOut();
-var nouns = [];
-var adjectives = [];
-var verbs = [];

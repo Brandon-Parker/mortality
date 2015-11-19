e0254c6faf8abc486faea89dd006803a9752f206
Sawako Ishida <sawako.yoriko@gmail.com>
2015-11-18 17:09:35 -0800

    populated madlib and added a button to clear loclstrg

--- js/results.js
+++ js/results.js
@@ -1,2 +1,14 @@
 var username = localStorage.hiphopopotamus;
 var madLib = localStorage.rhymenoceros;
+var greeting = document.getElementById("greeting");
+var madLibSection = document.getElementById("madLib");
+var goBack = document.getElementById("goBack");
+
+greeting.innerHTML =  "hello, " + username + ". Here is your mad lib!";
+madLibSection.innerHTML = madLib;
+
+goBack.addEventListener("click", function() {
+  localStorage.removeItem("hiphopopotamus");
+  localStorage.removeItem("rhymenoceros");
+  window.location = "../html/index.html";
+});
